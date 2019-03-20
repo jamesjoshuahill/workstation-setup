760bee2575d3d5f1522a1aae7754c59377a78a2a
Josh Hill <jhill@pivotal.io>
2019-03-20 16:24:26 +0000

    Pin kustomize to v1.0.11

--- scripts/opt-in/golang.sh
+++ scripts/opt-in/golang.sh
@@ -15,15 +15,15 @@ pushd ~/workspace/pivotal_ide_prefs/cli
 ./bin/ide_prefs install --ide=goland
 popd
 
-go get -u github.com/onsi/ginkgo/ginkgo
-go get -u github.com/maxbrunsfeld/counterfeiter
+### Tools for ISM
 
 install_kubebuilder() {
   local version=1.0.8 # latest stable version
   local arch=amd64
 
   # create temp dir
-  local dir="$(mktemp -d)"
+  local dir
+  dir=$(mktemp -d)
   pushd $dir
 
   # download the release
@@ -45,8 +45,28 @@ install_kubebuilder() {
   fi
 }
 
-install_kubebuilder
+install_kustomize() {
+  local version=1.0.11  # latest v1.x version
 
-brew install kustomize
+  # create temp dir
+  local dir
+  dir=$(mktemp -d)
+  pushd $dir
+
+  # download binary
+  wget "https://github.com/kubernetes-sigs/kustomize/releases/download/v${version}/kustomize_${version}_darwin_amd64"
+  install "kustomize_${version}_darwin_amd64" /usr/local/bin/kustomize
+
+  # clean up temp dir
+  popd
+  rm -rf $dir
+}
+
 brew cask install virtualbox
 brew cask install minikube
+
+go get -u github.com/onsi/ginkgo/ginkgo
+go get -u github.com/maxbrunsfeld/counterfeiter
+
+install_kubebuilder
+install_kustomize
